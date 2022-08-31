a7b078646a77cff6b88c9d2d80728024f95940b9
Braejan Arias <barias@sonatype.com>
2022-08-31 11:22:04 -0500

    :poop: Add smell code.

--- src/main/java/com/sample/lift/demo/util/CommonUtil.java
+++ src/main/java/com/sample/lift/demo/util/CommonUtil.java
@@ -1,17 +1,22 @@
 package com.sample.lift.demo.util;
 
+
+import java.io.File;
 import java.util.HashSet;
 import java.util.Set;
 
 public class CommonUtil {
 
 
-    public static void Test (){
-        Set<Short> s = new HashSet<>();
-        for (short i = 0; i < 100; i++) {
-            s.add(i);
-            s.remove(i - 1);
+    public static void OpenFile (){
+        try
+        {
+            File file = new File("C:\\demo\\demofile.txt");
         }
-        System.out.println(s.size());
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
     }
 }
