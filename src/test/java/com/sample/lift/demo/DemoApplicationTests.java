14818dc8b3b5ddcbd9ea5e7a8c77df725d9cf60a
Braejan Arias <barias@sonatype.com>
2022-10-05 12:57:46 -0500

    :poop: Bad libraries into a new Spring project.

---
+++ src/test/java/com/sample/lift/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.lift.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
